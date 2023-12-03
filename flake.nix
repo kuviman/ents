6c12718c95c76743cc6fc5112aa55725da1db41a
kuviman <kuviman@gmail.com>
2023-12-03 16:24:05 +0400

    Thing compiles

--- flake.nix
+++ flake.nix
@@ -12,17 +12,48 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        rust = (pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+          targets = [ "wasm32-unknown-unknown" ];
+        });
+
+        shellInputs = with pkgs; [
+          rust
+          clang
+          mold
+          trunk
+        ];
+        appNativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+        appBuildInputs = appRuntimeInputs ++ (with pkgs; [
+          udev
+          alsaLib
+          xorg.libX11
+          vulkan-tools
+          vulkan-headers
+          vulkan-validation-layers
+        ]);
+        appRuntimeInputs = with pkgs; [
+          udev
+          alsaLib
+          vulkan-loader
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+        ];
       in
       with pkgs;
       {
-        devShells.default = mkShell {
-          buildInputs = [
-            (rust-bin.stable.latest.default.override {
-              extensions = [ "rust-src" ];
-              targets = [ "wasm32-unknown-unknown" ];
-            })
-            trunk
-          ];
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = appNativeBuildInputs;
+          buildInputs = shellInputs ++ appBuildInputs;
+
+          shellHook = ''
+            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath appRuntimeInputs}"
+            ln -fsT ${rust} ./.direnv/rust
+          '';
         };
       }
     );
